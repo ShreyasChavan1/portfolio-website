a1a71b7bc5950052ed7a64f8fa83ecb6233f5dd6
ShreyasChavan1 <152523720+ShreyasChavan1@users.noreply.github.com>
2025-12-19 23:50:43 +0530

    added two skill secions

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -1,4 +1,4 @@
-"use-client"
+"use client"
 import Image from "next/image"
 import hero from "@/app/assets/4kfullbody (1).png"
 import hand from "@/app/assets/4khand.png"
@@ -6,49 +6,50 @@ import backfire from "@/app/assets/firecutout.png"
 import Aura from "@/app/assets/aura.png"
 import stars from "@/app/assets/stars.png"
 import Fire from "./fire"
+import About from "./about"
 export default function Hero() {
     return (
         <div className="relative w-full h-screen overflow-hidden bg-[#06090A]">
-            <div className="relative w-full h-screen max-w-[1500px] mx-auto px-8 flex flex-center">
+            <div className="relative w-full h-full max-w-[1500px] mx-auto px-8 flex">
 
-                {/* left text */}
-                <div className="text-[0px] mt-[50px] ml-[50px] w-[760px] h-[220px]  leading-[0.9]">
-                    
-                    <span className="text-[130px] text-[#9A6BFF] font-bold ">Hello</span><br></br>
-                    <span className="text-[110px] text-white font-bold ">Call me <br></br>Web-Devloper</span>
+                <div className="mt-13 ml-10 w-fit h-fit  leading-[0.8]">
+                    <span className="text-9xl text-[#9A6BFF] font-bold ">Hello</span><br></br>
+                    <span className="text-8xl text-white font-bold ">Call me <br></br>Web-Devloper</span>
                 </div>
             <div className="absolute bottom-0 right-0 pointer-events-none z-20">
-                <Image
-                    src={backfire}
-                    alt="backfire"
-                    width={500}
-                    className="absolute bottom-25 right-30 z-0"
-                />
-                <Image
-                    src={Aura}
-                    alt="Aura"
-                    width={600}
-                    className="absolute bottom-[-100] right-25 z-5"
-                />
-                <Image
-                    src={stars}
-                    alt="stars"
-                    width={550}
-                    className="absolute bottom-50 right-28 z-5"
-                />
-                <Image
-                    src={hero}
-                    alt="hero"
-                    width={780}
-                    height={500}
-                    className="relative z-10"
-                />
-                <Image
-                    src={hand}
-                    alt="hand"
-                    width={780}
-                    className="absolute bottom-0 right-18 z-20"
-                />
+                <div className="relative w-full h-full">
+                    <Image
+                        src={backfire}
+                        alt="backfire"
+                        width={500}
+                        className="absolute bottom-25 right-30 z-0"
+                    />
+                    <Image
+                        src={Aura}
+                        alt="Aura"
+                        width={600}
+                        className="absolute bottom-[-100] right-25 z-5"
+                    />
+                    <Image
+                        src={stars}
+                        alt="stars"
+                        width={550}
+                        className="absolute bottom-50 right-28 z-5"
+                    />
+                    <Image
+                        src={hero}
+                        alt="hero"
+                        width={780}
+                        height={500}
+                        className="relative z-10"
+                    />
+                    <Image
+                        src={hand}
+                        alt="hand"
+                        width={780}
+                        className="absolute bottom-0 right-18 z-20"
+                    />
+                </div>
             </div>
         </div>
     </div>
--- app/components/sections/skills.tsx
+++ app/components/sections/skills.tsx
@@ -1,8 +1,58 @@
+import { HtmlHTMLAttributes } from "react"
 
 export default function Skills(){
+    // type Types = {
+    //     category:string,
+    //     tech:string[],
+    // }
+    // const skills:Types[]=[
+    //     {
+    //         category:"LANGUAGES",
+    //         tech:["JAVASCRIPT","PYTHON","C++"]
+    //     },
+    //     {
+    //         category:"WEB-DEV",
+    //         tech:["MERN","NEXT.JS","HTML","TAILWIND","FRAMER-MOTION"]
+    //     },
+    //     {
+    //         category:"BACKEND",
+    //         tech:["SQL/NOSQL","REST APIS","WEBSOCKETS","JWT Auth","REDIS"]
+    //     },
+    //     {
+    //         category:"DEVOPS",
+    //         tech:["Github Actions","NGINX","AZURE","DOCKER","GIT"]
+    //     },
+
+    // ] 
+    // function custom_translate(word:string):string{
+    //     const size:number = word.length
+        
+    //     return ""
+    // }
     return(
-        <div className="h-screen w-full bg-[#06090A] overflow-hidden">
-            <h1>Hello world</h1>
+        <div className="h-full w-full bg-[rgb(6,9,10)] overflow-hidden">
+            <div className="relative h-full max-w-[1500px] p-6 mx-auto flex items-center justify-around">
+                <div className="[writing-mode:vertical-rl] rotate-180 text-9xl font-bold text-[#875edd]  relative">
+                        LANGUAGES
+                </div>
+                <div className="w-fit relative flex flex-col text-8xl font-bold items-end justify-center ">
+                    <span className="">JAVASCRIPT</span>
+                    <span className="-translate-x-80">PYTHON</span>
+                    <span className="-translate-x-150">C++</span>
+                </div>
+            </div>
+            <div className="relative h-full max-w-[1500px] p-6 mx-auto flex items-center justify-around">
+                <div className="w-fit relative flex flex-col text-8xl font-bold items-end justify-center ">
+                    <span className="-translate-x-120">MERN</span>
+                    <span className="-translate-x-100">NEXT</span>
+                    <span className="-translate-x-80">HTML</span>
+                    <span className="-translate-x-50">TAILWIND</span>
+                    <span className="">FRAMER-MOTION</span>
+                </div>
+                <div className="[writing-mode:vertical-rl] text-9xl font-bold text-[#875edd]  relative">
+                        WEB-DEV
+                </div>
+            </div>
         </div>
     )
 }
