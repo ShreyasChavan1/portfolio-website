d2f051321635f90f738de9fb0acc54d0dbe4e329
ShreyasChavan1 <152523720+ShreyasChavan1@users.noreply.github.com>
2026-02-03 12:58:45 +0530

    added some effect in hero

--- app/components/sections/hero.tsx
+++ app/components/sections/hero.tsx
@@ -5,9 +5,11 @@ import hand from "@/app/assets/4khand.png"
 import backfire from "@/app/assets/firecutout.png"
 import Aura from "@/app/assets/aura.png"
 import stars from "@/app/assets/stars.png"
-import { motion, useScroll, useTransform ,easeInOut } from 'framer-motion'
+import logo from "@/app/assets/image.png"
+import { motion, useScroll, useTransform ,easeInOut, useMotionValue, useSpring } from 'framer-motion'
 
 import type { Transition } from "framer-motion"
+import React, { useState } from "react"
 
 const EASE_IN_OUT: [number, number, number, number] = [0.4, 0, 0.2, 1]
 const LINEAR: [number, number, number, number] = [0, 0, 1, 1]
@@ -38,28 +40,72 @@ const STARS_ANIMATION = {
 
 
 export default function Hero() {
+
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const Handlehover = (e:React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect()
+      mouseX.set(e.clientX - rect.left),
+      mouseY.set(e.clientY - rect.top)
+    
+  }
+const x = useSpring(
+  useTransform(mouseX, [0, window.innerWidth], [-10, 10]),
+  { stiffness: 40, damping: 20 }
+);
+
+const y = useSpring(
+  useTransform(mouseY, [0, window.innerHeight], [-6, 6]),
+  { stiffness: 40, damping: 20 }
+);
+
   const MotionImage = motion(Image) //Wraps Next.js’s <Image> component with Framer Motion so it can animate.
   const { scrollYProgress } = useScroll()
   const progress = useTransform(scrollYProgress, [0, 0.35], [0, 1])
   const handY = useTransform(progress, [0, 1], ["0%", "-20%"])
 
   return (
-    <div className="relative w-full h-screen overflow-hidden bg-[#06090A]">
+    <div onMouseMove={Handlehover} className="relative w-full h-screen overflow-hidden bg-[#06090A]">
+      <motion.div
+  className="pointer-events-none absolute inset-0 hidden md:block"
+  style={{
+    background: useTransform(
+      [mouseX, mouseY],
+      ([x, y]) =>
+        `radial-gradient(
+          350px circle at ${x}px ${y}px,
+          rgba(135, 94, 221, 0.25),
+          rgba(6, 9, 10, 0.95) 70%
+        )`
+    ),
+  }}
+/>
+<pre className="absolute bottom-5 left-1/2 -translate-x-1/2 text-white text-2xl animate-pulse hidden md:block">
+Scroll
+  <span className="text-purple-400">↓</span>
+</pre>
+
+
       <div className="relative lg:h-full md:h-full max-w-[1500px] mx-auto px-2 lg:px-8 md:px-6 flex flex-col-reverse lg:flex-row items-center lg:items-start md:items-center">
 
         {/* Text */}
         <div className="mt-10 ml-3 md:ml-10 w-fit h-fit">
           <span className="text-[60px] lg:text-9xl md:text-8xl text-purple-400 font-bold leading-[0.8]">
-            Hello
+            I Build 
           </span><br />
-          <span className="text-[60px] lg:text-[120px] md:text-[100px] text-white font-bold leading-[0.9]">
-            Call me <br />Web-Developer
+          <span className="text-[60px] lg:text-[120px] md:text-8xl text-white font-bold leading-[0.9]">
+            Websites <br /> That Move
           </span>
+          {/* <div className="bg-purple-400 text-white w-fit p-3 text-5xl font-bold hover:bg-white hover:text-purple-400 transition-colors duration-300 ease-in-out cursor-pointer m-3">My Work</div> */}
         </div>
 
         {/* Desktop visuals */}
         <div className="absolute hidden lg:block md:block bottom-0 right-0 pointer-events-none z-20">
-          <div className="relative w-full h-full">
+          <motion.div 
+           style={{x,y}}
+  transition={{ type: "spring", stiffness: 40, damping: 20 }}
+          className="relative w-full h-full">
 
             <MotionImage
               src={backfire}
@@ -97,7 +143,7 @@ export default function Hero() {
               style={{ y: handY }}
             />
 
-          </div>
+          </motion.div>
         </div>
 
         {/* Mobile visuals */}
@@ -141,7 +187,14 @@ export default function Hero() {
           <div className="absolute -bottom-6 left-0 w-full h-[200px] z-30 bg-linear-to-t from-[#06090A] via-[#06090A]/60 to-transparent" />
 
         </div>
+        
+        <Image
+        src={logo}
+        alt="logo"
+        width={200}
+        className="absolute bottom-0 left-0"/>
       </div>
+
     </div>
   )
 }
